8d3dd6d8d07c7c4b212bf9d54a0fc49e157cf27d
oferro <ofer.rotshtein@gmial.com>
2021-05-09 00:47:19 +0300

    add state and SerchBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import SerchBox from './SerchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <CardList robots={robots} />
-    );
+
+class App extends Component  {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            serchfield: ''
+        }
+    }
+
+    onSerchChange = (event) => {
+        this.setState({serchfield: event.target.value}) 
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+                return robots.name.toLowerCase().includes(this.state.serchfield.toLowerCase());
+            })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SerchBox serchChange={this.onSerchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;
